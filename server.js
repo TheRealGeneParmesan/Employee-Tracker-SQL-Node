74e4a134110851ed9434343dd5376595a76a81d2
TheRealGeneParmesan <Geneparmigiano@gmail.com>
2023-03-13 21:47:35 -0700

    deleted the connections.js since we can't use sequelize, dotenv set up

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 // Importing the mysql & inquirer packages
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+require('dotenv').config();
 
 // Connect to database
 const db = mysql.createConnection({
     host: 'localhost',
     // MYSQL username & pass
-    user: 'root',
-    password: 'pNk9UeLmIY15!0IU',
-    database: 'employees_db'
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 db.connect((err) => {
